2f363db78cba3c07f611d3971953a844343d179c
Atharva Zaveri <atharvazaveri@Atharvas-MacBook-Pro.local>
2022-09-07 15:12:00 +0530

    c

---
+++ first.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main() {
+    int i,p,n,a[10];
+    printf("enter the size of the array:",n);
+    scanf("%d",&n);
+    printf("enter the elements:",a);
+    for(i=0;i<n;++i){
+        scanf("%d",&a[i]);
+    }
+    printf("enter the position of the element:",p);
+    scanf("%d",&p);
+    printf("the element is %d",a[p-1]);
+}
+
+    
+
+
+
+
+    
+    
+    
+        
+
+       
+
+
+    
+    
+    
+        
+    
+    
+
+
+
+   
+        
+   
+    
+   
